a21bf56163957082de5bdec2b2644822791a0028
MohammadFaysal731 <www.faysal731@gmail.com>
2023-04-24 19:55:33 +0600

    create update item api for add mark system

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express =require("express");
-const cors =require("cors");
+const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ClientSession, ObjectId } = require("mongodb");
-const app=express();
-const port =process.env.PORT ||5000;
+const {
+  MongoClient,
+  ServerApiVersion,
+  ClientSession,
+  ObjectId,
+} = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -19,46 +24,59 @@ const client = new MongoClient(uri, {
   },
 });
 async function run() {
-  
   try {
-    const itemsCollection = client.db('reminder').collection("items");
+    const itemsCollection = client.db("reminder").collection("items");
     //this api for get all items
-    app.get('/items', async(req,res)=>{
+    app.get("/items", async (req, res) => {
       const items = await itemsCollection.find().toArray();
       res.send(items);
-    })
+    });
     //this api for get single item
-    app.get('/item/:id', async(req,res)=>{
-      const id =req.params.id;
-      const query ={_id:new ObjectId(id)} 
+    app.get("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const item = await itemsCollection.findOne(query);
       res.send(item);
-    })
+    });
     //this api for post a item
-    app.post('/items',async(req,res)=>{
-      const itemData =req.body;
+    app.post("/items", async (req, res) => {
+      const itemData = req.body;
       const addedItem = await itemsCollection.insertOne(itemData);
       res.send(addedItem);
-    })
-    // this api for delete item 
-    app.delete('/item/:id',async(req,res)=>{
-      const id =req.params.id;
-      const query ={_id:new ObjectId(id)}
+    });
+    //this api for add make item
+    app.put("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          mark: "mark",
+        },
+      };
+      const markedItem = await itemsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(markedItem);
+    });
+    // this api for delete item
+    app.delete("/item/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
       const deletedItem = await itemsCollection.deleteOne(query);
-      res.send(deletedItem)
-    })
-  } 
-  finally {
-  
+      res.send(deletedItem);
+    });
+  } finally {
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hell from Reminder App");
+});
 
-app.get('/',(req,res)=>{
-  res.send("Hell from Reminder App")
-})
-
-app.listen(port,()=>{
+app.listen(port, () => {
   console.log(`Reminder App listening on port ${port}`);
 });
