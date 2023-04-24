7ffc36bbf11c6ae861b224d0bd79c2f4ebc079a6
MohammadFaysal731 <www.faysal731@gmail.com>
2023-04-24 18:10:32 +0600

    create single item api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express =require("express");
 const cors =require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ClientSession } = require("mongodb");
+const { MongoClient, ServerApiVersion, ClientSession, ObjectId } = require("mongodb");
 const app=express();
 const port =process.env.PORT ||5000;
 
@@ -26,6 +26,13 @@ async function run() {
     app.get('/items', async(req,res)=>{
       const items = await itemsCollection.find().toArray();
       res.send(items);
+    })
+    //this api for get all items
+    app.get('/item/:id', async(req,res)=>{
+      const id =req.params.id;
+      const query ={_id:new ObjectId(id)} 
+      const item = await itemsCollection.findOne(query);
+      res.send(item);
     })
     //this api for post a item
     app.post('/items',async(req,res)=>{
